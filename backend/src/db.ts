34f10646f6894d4a6f98959222fd974c4d65ff3a
Rogelio <jonex720@gmail.com>
2020-12-08 13:33:46 -0600

    added Server's and DB's configuration

---
+++ backend/src/db.ts
@@ -0,0 +1,35 @@
+//Environment Variables
+require('dotenv').config();
+
+// Dependences
+import { createConnection, ConnectionOptions, Connection } from "typeorm";
+
+
+const newConnection = async (options?: ConnectionOptions): Promise<void> => {
+    // Local varibles
+    let connection: Connection;
+
+    try {
+        // Create a connection to the Data Base, with commun settings or user settings
+        if (options)
+            connection = await createConnection(options);
+        else
+            connection = await createConnection({
+                type: "mysql",
+                url: process.env.DB_URL,
+                entities: process.env.NODE_ENV == "dev" ? ["../src/models/**/*.ts"] : ["./dist/models/**/*.js"],
+                synchronize: true
+            });
+
+        // It validates that the connection to the database is done
+        if (connection)
+            console.log(">> DB is connected");
+    } catch (error) {
+        console.error(error);
+    }
+
+};
+
+// Export module
+export default newConnection;
+
